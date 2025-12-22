1b5c0b73f8474da6662e3251010b30e88393122c
PremoHD <rd86c2s5qh@privaterelay.appleid.com>
2025-12-22 05:55:08 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configuration file
+
+NETWORK = 'testnet'  # Change to 'bitcoin' for mainnet
+WALLET_NAME = 'JustusCodeSpaceWallet'
+
+# Optional: default recipient addresses (for testing batch sends)
+DEFAULT_ADDRESSES = [
+    "tb1qexampleaddress1",
+    "tb1qexampleaddress2"
+]
+DEFAULT_AMOUNT = 0.00001  # BTC per address
